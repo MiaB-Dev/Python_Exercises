6abff36e4828873b3abfe99b76ecde8009de31fa
MiaB-Dev <mia.pbueno@gmail.com>
2023-05-30 16:49:08 -0300

    calc v2

--- calc/calc.py
+++ calc/calc.py
@@ -1,41 +1,55 @@
+from math import sqrt
+
 def main():
     
-    n1 = input("Insert a number: ")
-    if n1.isdigit():
-        n1 = int(n1)
-    else:
-        n1 = float(n1)
+    n1 = read_num()
 
-    op = input("Choose an operator: + - * /: ")
-    
-    n2 = input("Insert a number: ")
-    if n2.isdigit():
-        n2 = int(n2)
-    else:
-        n2 = float(n2)
+    op = input("Choose an operator: + - * / ^ √ ")
+
         
     match op:
         case "+":
+            n2 = read_num()
             result = n1 + n2
         
         case "-":
+            n2 = read_num()
             result = n1 - n2
         
         case "*":
+            n2 = read_num()
             result = n1 * n2
         
         case "/":
+            n2 = read_num()
             if (n2 == 0):
                 print ("not possible to divide by 0!")
                 result = "ERROR"
             else:
                 result = n1 / n2
                 
+        case "^":
+            n2 = read_num()
+            result = pow(n1, n2)
+
+        case "√":
+            result = sqrt(n1)
+
         case _:
             print("invalid operator")
     
     
     print (result)
+
+def read_num():
+    n = input("Insert a number: ")
+    if n.isdigit():
+        n = int(n)
+    else:
+        n = float(n)
+    
+    return n
+
     
 
 if __name__ == "__main__":
